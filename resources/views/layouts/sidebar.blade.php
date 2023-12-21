2515b4444053f39519a3e6fe44f919ed8354eb4f
aymantech1 <rakibhossincse@gmail.com>
2023-12-21 17:38:35 +0600

    sidebar image changed

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -23,7 +23,7 @@ $userId = auth()->user()->id;
                 <img src="{{ URL::asset('assets/images/logo-sm.png') }}" alt="" height="22">
             </span>
             <span class="logo-lg">
-                <img src="{{ URL::asset('assets/images/logolight.png') }}" alt="" height="17">
+                <img src="{{ URL::asset('assets/images/logolight-s.png') }}" alt="" height="24">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
